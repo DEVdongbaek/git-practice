32f001281925a0009d2dc97bf45ee66c9261fcef
DEVdongbaek <yardyard@likelion.org>
2023-04-24 21:36:54 +0900

    feat: Print fizz
    Condition: i is multiple of 3
    Otherwise, print i itself

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,10 @@
-def do_fizzbuzz():
-    print('hello')
+def do_fizzbuzz(num:int):
+    for i in range(1, num+1):
+        if i%3 == 0:
+            print('hello')
+        else:
+            print(f'{i}')
 
+`
 if __name__ == '__main__':
-    do_fizzbuzz()
+    do_fizzbuzz(16)
